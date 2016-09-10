18665ec73ea9094167ed61bbc57b18aa6c220df0
Joshua Jahans <joshua.jahans@thehutgroup.com>
2016-09-10 15:12:32 +0100

    Setup: gulp tasks, sandbox server, sourcemaps, folder structure.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Zyperjs
-Enlarge text entered into text fields for those with poor eyesight
+Enlarge text entered into text fields primarily for those with poor eyesight but also suitable just for emphasis.
---
+++ zyper/zyper.js
@@ -0,0 +1,19 @@
+'use strict';
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+/**
+ * Created by jahansj on 10/09/2016.
+ */
+
+// Must take config object, first option is to target all text input or only a specific class 
+// if no specific class provided uses default .zyper (or something)
+
+var zyper = function zyper(options) {
+  _classCallCheck(this, zyper);
+
+  console.log('init');
+};
+
+new zyper();
+//# sourceMappingURL=zyper.js.map
